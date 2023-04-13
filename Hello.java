cd580410089a96efdffc1f47bf88dfe73fbd71d3
ohyoungmin <yoengmin@naver.com>
2023-04-13 10:22:39 +0900

    first

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//  this jave file
+puvlic class Hello {
+     public static void main(String[] args) {
+     	System.out.println("Hello korea");
+     	system.out.println(20 + 30);
+
+     }
+}
---
+++ hello_o1/src/com/callor/hello/helloYour.java
@@ -0,0 +1,18 @@
+package com.callor.hello;
+/*
+ * Source Code, java Class File
+ */
+
+
+
+
+public class helloYour{
+	public static void main(String[] args) {
+		System.out.println(30+40);
+		System.out.println(30-40);
+		System.out.println(30*40);
+		System.out.println(30/40);
+		System.out.println(30%40);
+	}
+
+}
